9acbc9a89c42dec5005d33b926caa60af0e06436
diego0096 <dfqz93@hotmail.com>
2020-02-04 01:24:57 +0000

    first commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,23 +9,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t c = size;
-    size_t i, t = 0;
+	size_t c = size;
+	size_t i, t = 0;
 
-    if (array == NULL || size == 0)
-        return;
-    while (c > 0)
-    {
-        for (i = 0; i < size - 1; i++)
-    {
-            if (array[i] > array[i + 1])
-            {
-                t = array[i + 1];
-                array[i + 1] = array[i];
-                array[i] = t;
-                print_array(array, size);
-            }
-        }
-        c--;
-    }
+	if (array == NULL || size == 0)
+		return;
+	while (c > 0)
+	{
+		for (i = 0; i < size - 1; i++)
+	{
+			if (array[i] > array[i + 1])
+			{
+				t = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = t;
+				print_array(array, size);
+			}
+		}
+		c--;
+	}
 }
